9e501bd06a16276cba943d529d0bf0e050b1f1d4
yteja08 <y.teja0804@gmail.com>
2026-07-12 23:34:59 +0530

    Initial commit

---
+++ Verilog_Language/Basics/chip_7456.v
@@ -0,0 +1,17 @@
+module top_module ( 
+    input p1a, p1b, p1c, p1d, p1e, p1f,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+    
+    wire and1,and2,and3,and4;
+
+    assign and1=p1a&p1b&p1c;
+    assign and2=p1d&p1e&p1f;
+    assign p1y=and1|and2;
+    
+    assign and3=p2a&p2b;
+    assign and4=p2c&p2d;
+    assign p2y=and3|and4;
+
+endmodule
