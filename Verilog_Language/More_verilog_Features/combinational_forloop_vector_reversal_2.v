4b5f9c9240884b37f6906e02c3838cee4c2cd0d2
yteja08 <y.teja0804@gmail.com>
2026-07-13 18:15:22 +0530

    Reduction for wider gates done

---
+++ Verilog_Language/More_verilog_Features/combinational_forloop_vector_reversal_2.v
@@ -0,0 +1,12 @@
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+);
+    integer i;
+    always @(*) begin
+        for(i=0;i<=99;i++)
+            out[i]=in[99-i];
+    end
+
+endmodule
+
