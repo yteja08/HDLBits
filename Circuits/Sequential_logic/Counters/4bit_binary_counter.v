f0cf12bfcf9ee55692b27c41cc13780fd3c8affe
yteja08 <y.teja0804@gmail.com>
2026-07-20 11:36:12 +0530

    Today's completion added successfully

---
+++ Circuits/Sequential_logic/Counters/4bit_binary_counter.v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input reset,      // Synchronous active-high reset
+    output [3:0] q);
+    
+    integer i;
+    integer count;
+    
+    always @(posedge clk) begin
+        if (reset)
+            q<=4'd0;
+        else
+            q<=q+1;
+    end
+
+endmodule
