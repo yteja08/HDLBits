94b358f13317b1954d668e641111777aa5c9c728
yteja08 <y.teja0804@gmail.com>
2026-08-03 23:32:18 +0530

    Added shift register hdlbits

---
+++ Circuits/Sequential_logic/Shift_register/left_right_arithmetic_shift.v
@@ -0,0 +1,25 @@
+module top_module(
+    input clk,
+    input load,
+    input ena,
+    input [1:0] amount,
+    input [63:0] data,
+    output reg signed [63:0] q); 
+    always @(posedge clk)begin
+        if(load)
+            q<=data;
+        else begin
+            if(ena) begin
+                casex(amount)
+                    2'b00:q<=q<<<1;
+                    2'b01:q<=q<<<8;
+                    2'b10:q<=q>>>1;
+                    2'b11:q<=q>>>8;
+                    default:q<=q;
+                endcase
+            end
+        end
+
+     end
+
+endmodule
